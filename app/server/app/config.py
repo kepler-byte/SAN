f476a4cbf920b83905794396ea5faf513510d4e5
Kepler Dev <68844047+kepler-byte@users.noreply.github.com>
2025-09-02 13:37:31 +0000

    REset

--- app/server/app/config.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-MONGODB_URL = os.getenv("MONGODB_URL")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
--- app/server/app/models/user.py
+++
@@ -1,9 +0,0 @@
-from beanie import Document
-from pydantic import Field
-
-class User(Document):
-    name: str
-    email: str
-
-    class Settings:
-        name = "users"  # ชื่อ collection ใน MongoDB
