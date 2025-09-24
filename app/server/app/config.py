f5c52f9e1425dd788b699ae5835b783c3749d8f0
jittratep <jittratep@gmail.com>
2025-09-24 09:14:33 +0700

    fix: update dotenv loading to specify the path for environment variables

--- app/server/app/config.py
+++ app/server/app/config.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path=dotenv_path)
 
 MONGO_URI = os.getenv("MONGO_URI")
 SECRET_KEY = os.getenv("SECRET_KEY")
